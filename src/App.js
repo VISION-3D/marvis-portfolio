0f192bba631c7211520dcbbb8df7cbfed58757e7
VISION-3D SENE <ssene07@gmail.com>
2025-11-28 20:53:55 +0100

    Ajout SEO: title, meta description, JSON-LD et H1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import AOS from "aos";
 
 import Navbar from "./components/Navbar";
@@ -12,17 +13,35 @@ import Contact from "./pages/Contact";
 
 function App() {
   useEffect(() => {
-    AOS.init({ 
-      duration: 1000, 
-      once: true, 
-      mirror: false,
-      offset: 100
-    });
+    AOS.init({ duration: 1000, once: true, mirror: false, offset: 100 });
     AOS.refresh();
   }, []);
 
   return (
     <Router>
+      <Helmet>
+        <title>TaqwaTech – Développement Web & IA</title>
+        <meta
+          name="description"
+          content="TaqwaTech propose des solutions innovantes en développement web, mobile et intelligence artificielle pour entreprises et startups."
+        />
+        <meta name="keywords" content="développeur web Sénégal, développeur full stack, intelligence artificielle, agence web, création site web, applications web et mobile" />
+        {/* JSON-LD Schema */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "TaqwaTech",
+            "url": "https://taqwatech.vercel.app/",
+            "logo": "https://taqwatech.vercel.app/favicon.ico",
+            "sameAs": [
+              "https://www.linkedin.com/in/taqwatech",
+              "https://twitter.com/taqwatech"
+            ]
+          })}
+        </script>
+      </Helmet>
+
       <Navbar />
       <main style={{ paddingTop: "0px" }}>
         <Routes>
